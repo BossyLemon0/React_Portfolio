87ead4b49a1c719c1f3f1645fed94e053d2287be
Omar Ramirez <omardramirez2002@gmail.com>
2024-06-01 16:25:15 -0600

    Adds the hover animations for skills, needs tweaking

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,45 @@
-import React from "react";
+import React, { useState } from 'react';
+import { debounce } from 'lodash';
 // import '../styles/App.css';
 
 export default function About(props){
+
+    const [hoveredRow, setHoveredRow] = useState(null);
+
+    function handleMouseOver(event, row) {
+        // console.log(event.target.parentNode.parentNode.getBoundingClientRect())
+        // console.log(event.currentTarget.parentNode.parentNode.getBoundingClientRect())
+        // const rect = event.currentTarget.getBoundingClientRect();
+        // console.log(rect)
+        // const mouseX = event.clientX;
+        // const rowMidpoint = rect.left + rect.width / 2;
+        // const distanceFromMidpoint = mouseX - rowMidpoint;
+
+
+
+
+        const skillsBox = event.target.parentNode.parentNode.getBoundingClientRect();
+        const mouseX = event.clientX;
+        const boxMidpoint = skillsBox.left + skillsBox.width / 2;
+
+        // console.log(boxMidpoint)
+        // console.log(mouseX)
+        if (mouseX < boxMidpoint) {
+            setHoveredRow('left');
+        }
+        else if(mouseX > boxMidpoint){
+            setHoveredRow('right');
+        }
+        else {
+            setHoveredRow(null)
+        }
+    }
+
+    // const debounceMouseOver = debounce(handleMouseOver, 40);
+    function handleMouseOut() {
+        setHoveredRow(null);
+    }
+
     return (
     <div className = 'slides about' name='about'>
         <div className="titles titleAbout">
@@ -16,32 +54,42 @@ export default function About(props){
                         Skills
                     </div>
                 </div>
-                <div className="skillsContentContainer">
-                    <div className="skillRow">
+                <div className={`skillsContentContainer`}>
+                    <div className={`skillRow ${hoveredRow === 'left' ? 'hover-left1' : ''} ${hoveredRow === 'right' ? 'hover-right1' : ''}`}
+                    onMouseOver={(event) => handleMouseOver(event)}
+                    onMouseOut={handleMouseOut}>
                         <div className="skillBox">HTML</div>
                         <div className="skillBox">Javascript</div>
                         <div className="skillBox">CSS</div>
                         <div className="skillBox">NoSQL</div>
                     </div>
-                    <div className="skillRow staggerLeft">
+                    <div className={` skillRow staggerLeft ${hoveredRow === 'left' ? 'hover-left2' : ''} ${hoveredRow === 'right' ? 'hover-right2' : ''}`}
+                    onMouseOver={(event) => handleMouseOver(event)}
+                    onMouseOut={handleMouseOut}>
                         <div className="skillBox">Macros</div>
                         <div className="skillBox">React</div>
                         <div className="skillBox">SQL</div>
                         <div className="skillBox">C#</div>
                     </div>
-                    <div className="skillRow staggerRight">
+                    <div className={`skillRow staggerRight ${hoveredRow === 'left' ? 'hover-left3' : ''} ${hoveredRow === 'right' ? 'hover-right3' : ''}`}
+                    onMouseOver={(event) => handleMouseOver(event)}
+                    onMouseOut={handleMouseOut}>
                         <div className="skillBox">Lua</div>
                         <div className="skillBox">Python</div>
                         <div className="skillBox">Docker</div>
                         <div className="skillBox">Cypress</div>
                     </div>
-                    <div className="skillRow staggerLeftMore">
+                    <div className={`skillRow staggerLeftMore ${hoveredRow === 'left' ? 'hover-left4' : ''} ${hoveredRow === 'right' ? 'hover-right4' : ''}`}
+                    onMouseOver={(event) => handleMouseOver(event)}
+                    onMouseOut={handleMouseOut}>
                         <div className="skillBox">Figma</div>
                         <div className="skillBox">Node.js</div>
                         <div className="skillBox">REST</div>
                         <div className="skillBox">Socket.io</div>
                     </div>
-                    <div className="skillRow staggerRightMore">
+                    <div className={`skillRow staggerRightMore ${hoveredRow === 'left' ? 'hover-left5' : ''} ${hoveredRow === 'right' ? 'hover-right5' : ''}`} 
+                    onMouseOver={(event) => handleMouseOver(event)}
+                    onMouseOut={handleMouseOut}>
                         <div className="skillBox">Express</div>
                         <div className="skillBox">Insomnia</div>
                         <div className="skillBox">Axios</div>
