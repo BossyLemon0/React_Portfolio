432943fd7263c8f916a4151eb77b12fbbaf0a71a
Omar Ramirez <omardramirez2002@gmail.com>
2024-06-03 01:04:29 -0600

    Adds the reflection and upper case to start

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Home(props){
     return (
     <div className = 'slides home'>
-        <div className ='first_slide isOn' name="home">Omar Ramirez</div>
+        <div className ='first_slide isOn' name="home">OMAR RAMIREZ</div>
     </div>
     );
 }
