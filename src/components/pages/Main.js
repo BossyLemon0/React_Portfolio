5e47815dd2ad0d281952e8f70906fcbc83849f83
Omar Ramirez <omardramirez2002@gmail.com>
2024-06-03 23:21:33 -0600

    uncovering lazy loaded use cases, more user friendly

--- src/components/pages/Main.js
+++ src/components/pages/Main.js
@@ -1,6 +1,7 @@
 import React, {lazy, Suspense} from "react";
 import '../../styles/App.css'
-const HomeComponent = lazy(()=> import('../Home'))
+import Home from '../Home'
+// const HomeComponent = lazy(()=> import('../Home'))
 const AboutComponent = lazy(()=> import('../About'))
 const ProjectsComponent = lazy(()=> import('../Projects'))
 const FooterComponent = lazy(()=> import('../Footer'))
@@ -11,12 +12,15 @@ const renderLoader = () => <p>Loading...</p>
 
 export default function Main(props){
 return( 
+    <>
+    <Home />
     <Suspense fallback = {renderLoader()}>
-        <HomeComponent />
+        {/* <HomeComponent /> */}
         <AboutComponent />
         <ProjectsComponent />
         <ContactComponent  />
         <FooterComponent />
     </Suspense>
+    </>
 );
 }
