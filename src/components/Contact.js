bbf05ea9470d41fc462d4e8d7815e4da3a485ad1
Omar Ramirez <omardramirez2002@gmail.com>
2024-06-05 01:50:38 -0600

    Handles conditional rendering on from submission

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,83 +4,60 @@ import '../styles/Form.css';
 
 export default function Contact(props){
     const [state, handleSubmit] = useForm("mzbnlkqo");
-    if (state.succeeded) {
-        return <p>Thanks for joining!</p>;
-    }
+    // if (state.succeeded) {
+    //     return <p>Thanks for joining!</p>;
+    // }
     return (
     <div className = 'slides contact'>
         <div className= 'titles'>Contact Me</div>
-        <form onSubmit={handleSubmit} method="POST">
-            <div className="name">
-                <input
-                    className="form-input"
-                    type="text"
-                    placeholder="Name"
-                    required  
-                    name="name"
-                ></input>
-            </div>
-            <div className="email">
-                <input
-                    className="form-input"
-                    type="email"
-                    placeholder="Email"
-                    required
-                    name="email"
-                ></input>
-                <ValidationError 
-                    prefix="Email" 
-                    field="email"
-                    errors={state.errors}
-                />
-            </div>
-            <div className="message">
-                {/* <label
-                    className="form-input"
-                    type="date"
-                    placeholder="email"
-                    required
-                ></label> */}
-                <textarea
-                    className="form-input"
-                    type="text"
-                    placeholder="Message"
-                    required
-                    name="message"
-                ></textarea>
-            </div>
-            <button type = 'submit' className = 'submit_btn' disabled={state.submitting}>
-                submit
-            </button>
-        </form>
-
-        {/* <form onSubmit={handleSubmit}>
-      <label htmlFor="email">
-        Email Address
-      </label>
-      <input
-        id="email"
-        type="email" 
-        name="email"
-      />
-      <ValidationError 
-        prefix="Email" 
-        field="email"
-        errors={state.errors}
-      />
-      <textarea
-        id="message"
-        name="message"
-      />
-      <ValidationError 
-        prefix="Message" 
-        field="message"
-        errors={state.errors}
-      />
-      <button type="submit" disabled={state.submitting}>
-        Submit
-      </button>
-    </form> */}
+        {
+            state.succeeded ?
+             <div className="titles">LETS'S GLOW!</div> 
+             :
+             <form onSubmit={handleSubmit}>
+             <div className="name">
+                 <input
+                     className="form-input"
+                     type="text"
+                     placeholder="Name"
+                     required  
+                     name="name"
+                 ></input>
+             </div>
+             <div className="email">
+                 <input
+                     className="form-input"
+                     type="email"
+                     placeholder="Email"
+                     required
+                     name="email"
+                 ></input>
+                 <ValidationError 
+                     prefix="Email" 
+                     field="email"
+                     errors={state.errors}
+                 />
+             </div>
+             <div className="message">
+                 {/* <label
+                     className="form-input"
+                     type="date"
+                     placeholder="email"
+                     required
+                 ></label> */}
+                 <textarea
+                     className="form-input"
+                     type="text"
+                     placeholder="Message"
+                     required
+                     name="message"
+                 ></textarea>
+             </div>
+             <button type = 'submit' className = 'submit_btn' disabled={state.submitting}>
+                 submit
+             </button>
+         </form>
+        }
     </div>
     );
 }
