35abc1c74bbad889e949a30e5312df983fac0945
Omar Ramirez <omardramirez2002@gmail.com>
2024-06-05 01:18:19 -0600

    Adds the formspree submissions

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,12 +1,17 @@
 import React from "react";
+import { useForm, ValidationError } from '@formspree/react';
 import '../styles/Form.css';
 
 export default function Contact(props){
+    const [state, handleSubmit] = useForm("mzbnlkqo");
+    if (state.succeeded) {
+        return <p>Thanks for joining!</p>;
+    }
     return (
     <div className = 'slides contact'>
         <div className= 'titles'>Contact Me</div>
-        <form action="mailto:omardramirez2002@gmail.com">
-            <div>
+        <form onSubmit={handleSubmit}>
+            <div className="name">
                 <label for=""></label>
                 <input
                     className="form-input"
@@ -15,16 +20,23 @@ export default function Contact(props){
                     required  
                 ></input>
             </div>
-            <div>
-                <label for=""></label>
+            <div className="email">
+                <label for="email">
+                    Email 
+                </label>
                 <input
                     className="form-input"
                     type="email"
                     placeholder="Email"
                     required
                 ></input>
+                <ValidationError 
+                    prefix="Email" 
+                    field="email"
+                    errors={state.errors}
+                />
             </div>
-            <div>
+            <div className="message">
                 <label
                     className="form-input"
                     type="date"
@@ -38,10 +50,38 @@ export default function Contact(props){
                     required
                 ></textarea>
             </div>
-            <button type = 'submit' className = 'submit_btn'>
+            <button type = 'submit' className = 'submit_btn' disabled={state.submitting}>
                 submit
             </button>
         </form>
+
+        {/* <form onSubmit={handleSubmit}>
+      <label htmlFor="email">
+        Email Address
+      </label>
+      <input
+        id="email"
+        type="email" 
+        name="email"
+      />
+      <ValidationError 
+        prefix="Email" 
+        field="email"
+        errors={state.errors}
+      />
+      <textarea
+        id="message"
+        name="message"
+      />
+      <ValidationError 
+        prefix="Message" 
+        field="message"
+        errors={state.errors}
+      />
+      <button type="submit" disabled={state.submitting}>
+        Submit
+      </button>
+    </form> */}
     </div>
     );
 }
