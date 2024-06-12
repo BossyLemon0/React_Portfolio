fef3716e9f1d67b0166245a24928adc1653d85ca
Omar Ramirez <omardramirez2002@gmail.com>
2024-06-12 00:44:26 -0600

    adds icon for menu

--- src/components/NavMobMenu.js
+++ src/components/NavMobMenu.js
@@ -2,6 +2,7 @@ import * as React from 'react';
 import Button from '@mui/material/Button';
 import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
+import MenuIcon from '@mui/icons-material/Menu';
 
 export default function NavMobMenu(props) {
   const elScroll = props.props
@@ -27,23 +28,44 @@ export default function NavMobMenu(props) {
         aria-haspopup="true"
         aria-expanded={open ? 'true' : undefined}
         onClick={handleClick}
+        sx={{ backgroundColor: 'transparent', color: 'white' }}
       >
-        Dashboard
+        <MenuIcon/>
       </Button>
       <Menu
         id="basic-menu"
         anchorEl={anchorEl}
         open={open}
         onClose={handleClose}
+        // sx={{ backgroundColor: 'black'}}
         MenuListProps={{
           'aria-labelledby': 'basic-button',
         }}
+        slotProps={{
+          paper:{
+            sx: {
+              backgroundColor: 'transparent',
+            }
+          }
+
+        }}
+
       >
-        <MenuItem onClick={handleClickItem} id = {'one'} >Home</MenuItem>
-        <MenuItem onClick={handleClickItem} id = {'two'} >About</MenuItem>
-        <MenuItem onClick={handleClickItem} id = {'three'} >Projects</MenuItem>
-        <MenuItem onClick={handleClickItem} id = {'four'} >Contact</MenuItem>
-        <MenuItem onClick={handleClickItem} id = {'five'} >Resume</MenuItem>
+        <MenuItem onClick={handleClickItem} 
+        sx={{ backgroundColor: 'transparent', color: 'white' }}
+         id = {'one'} >Home</MenuItem>
+                 <MenuItem onClick={handleClickItem} 
+        sx={{ backgroundColor: 'transparent', color: 'white' }}
+         id = {'two'} >About</MenuItem>
+                 <MenuItem onClick={handleClickItem} 
+        sx={{ backgroundColor: 'transparent', color: 'white' }}
+         id = {'three'} >Projects</MenuItem>
+                 <MenuItem onClick={handleClickItem} 
+        sx={{ backgroundColor: 'transparent', color: 'white' }}
+         id = {'four'} >Contact</MenuItem>
+                 <MenuItem onClick={handleClickItem} 
+        sx={{ backgroundColor: 'transparent', color: 'white' }}
+         id = {'five'} >Resume</MenuItem>
       </Menu>
     </div>
   );
